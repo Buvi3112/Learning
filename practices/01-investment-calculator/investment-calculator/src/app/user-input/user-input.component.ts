af2f1061200ce58d52f1ae954f4ee0f5268a4bfe
buvana <buvana31.12@outlook.in>
2025-07-25 18:50:39 +0530

    inverstment-calculator : two way binding, event handling

--- practices/01-investment-calculator/investment-calculator/src/app/user-input/user-input.component.ts
+++ practices/01-investment-calculator/investment-calculator/src/app/user-input/user-input.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<{
+    initialInvestment: number, 
+    duration: number, 
+    expectedReturn: number, 
+    annualInvestment: number
+  }>()
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExtenderReturn = '0';
+  enteredDuration = '10';
 
+  onSubmit() {
+    console.log("------------------")
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,  // adding + before string to convert string to number
+      duration: +this.enteredDuration, 
+      expectedReturn: +this.enteredExtenderReturn, 
+      annualInvestment: +this.enteredAnnualInvestment})
+  }
 }
